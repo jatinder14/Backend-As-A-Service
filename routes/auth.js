424330fe6452a6f75bb63142df9d6aaeaaf9b21d
jatinder14 <jatinder1901243@gmail.com>
2025-01-29 17:08:14 +0530

    fix login issue

--- routes/auth.js
+++ routes/auth.js
@@ -32,7 +32,7 @@ router.post('/login', async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        const user = await User.findOne({ email });
+        const user = await User.findOne({ email }).select('+password');
         if (!user) {
             return res.status(400).json({ message: 'User not found' });
         }

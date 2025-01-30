79ca2d4f8705540057ab3191fa3f744b21e6f1f7
jatinder14 <jatinder1901243@gmail.com>
2025-01-30 15:59:18 +0530

    fixed required issue

--- models/Blog.js
+++ models/Blog.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const blogSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    image: { type: String, required: true },
-    video: { type: String, required: true },
+    image: { type: String },
+    video: { type: String },
     date: { type: String, required: true },
     description: { type: String, required: true },
 }, { timestamps: true });

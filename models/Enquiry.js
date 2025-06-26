f8691df3da1e9acdd43c9e01bd6cdb126d50ea5d
jatinder14 <jatinder1901243@gmail.com>
2025-06-26 12:30:00 +0530

    added refresh token feature

--- models/Enquiry.js
+++ models/Enquiry.js
@@ -7,9 +7,13 @@ const enquirySchema = new mongoose.Schema({
     message: { type: String },
     price: { type: String },
     status: { type: String },
-    propertyId: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Property' }]
+    propertyId: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Property' }],
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    updatedBy: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
     // status: { type: String, enum: ['Active', 'Inprogress', 'Rejected'], default: 'Active' }
 
-});
+},
+    { timestamps: true }
+);
 
 module.exports = mongoose.model('Enquiry', enquirySchema);
